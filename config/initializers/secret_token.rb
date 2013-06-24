e5e3d2acdd9bca9a6e9a2e9d8a3f52f2302ce1fd
Tom Whyntie <twhyntie@gmail.com>
2013-06-24 19:03:51 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AwsTestApp3::Application.config.secret_token = '728a2fdc9fe14a9cf403528b23a2dcd389861aa659a5d6128b1eca168588060be8a38dd8f3e9e46d1310410227f59c9df1a9ffdcec9d5965f5cc2989356ecd35'
